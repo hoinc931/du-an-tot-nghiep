49db738b80f33c34c6ddb1bb9264fc406e625ec8
hoinc931 <c.hoivp55@gmail.com>
2022-01-10 09:20:09 +0700

    update

--- src/component/variableCommon.tsx
+++ src/component/variableCommon.tsx
@@ -4,6 +4,7 @@ export const variableCommon = {
     statusS: "successfully",
     statusF: "failed",
     localhost: "https://music-game-api-v1.herokuapp.com",
+    // localhost: "http://localhost:5000",
     extensionAudio: ["mp3", "wav", "flac", "aac"],
     extensionImage: ["jpg", "jpeg", "bmp", "gif", "png", "PNG"],
     sizeDefault: 16 * 1000 * 1000
